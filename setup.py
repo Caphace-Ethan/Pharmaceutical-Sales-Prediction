e7b30326be53fd9b9a5617cc7b1e8e4d4e318fda
Caphace-Ethan <ethanyoung135@gmail.com>
2021-07-26 21:49:25 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['pandas>=1.1.0', 'numpy>=1.19.0', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Ethani Caphace",
+    email="ethancaphace@gmail.com",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: Junior ML Engineer',
+        'Intended Audience :: World',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Ad campaign performance Testing using A/B Hypothesis Testing",
+    install_requires=requirements,
+    long_description=readme,
+    include_package_data=True,
+    keywords='scripts',
+    name='scripts',
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Caphace-Ethan/Ad-campaign-performance',
+    version='0.1.0',
+    zip_safe=False,
+)
