fda00590b2a9a72006aaca9ff2094d167328f36c
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-01 16:24:29 +0300

    Modified the Dashboard app.py

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashbo
 
 # loading the trained model
 pickle_in = open('models/01-08-2021-02-51-29-864.pkl', 'rb') 
-classifier = pickle.load(pickle_in)
+calculator = pickle.load(pickle_in)
  
 @st.cache()
 
 # defining the function which will make the prediction using the data which the user inputs 
-def prediction(Store, DayOfWeek, Promo, StateHoliday, Year, Month, Day, 
+def prediction(Store, StoreType, DayOfWeek, Promo, StateHoliday, Year, Month, Day, Open, Weekofmonth, SchoolHoliday,
                 WeekOfYear, days_to_from_hol, Assortment, CompetitionDistance, CompetitionOpenSinceMonth,
-                CompetitionOpenSinceYear, Promo2, Promo2SinceWeek, Promo2SinceYear):   
+                CompetitionOpenSinceYear, Promo2, Promo2SinceWeek, Promo2SinceYear, PromoInterval):   
  
     # Pre-processing user input    
     if Promo == 1:
@@ -36,6 +36,15 @@ def prediction(Store, DayOfWeek, Promo, StateHoliday, Year, Month, Day,
     else:
         StateHoliday = 0
     
+    if StoreType == "a":
+        StoreType = 1
+    elif StoreType == "b":
+        StoreType = 2
+    elif StoreType == "c":
+        StoreType = 3
+    else:
+        StoreType = 4
+
     if Assortment == "a":
         Assortment = 0
     elif Assortment == "b":
@@ -57,18 +66,16 @@ def prediction(Store, DayOfWeek, Promo, StateHoliday, Year, Month, Day,
         Promo2SinceYear = 1
     else:
         Promo2SinceYear = 0 
+
+
  
     # Making predictions 
-    prediction = classifier.predict( 
-        [[Store, DayOfWeek, Promo, StateHoliday, Year, Month, Day, WeekOfYear, 
-        days_to_from_hol, Assortment, CompetitionDistance, CompetitionOpenSinceMonth,
-        CompetitionOpenSinceYear, Promo2, Promo2SinceWeek, Promo2SinceYear]])
+    prediction = calculator.predict( 
+        [[Store, StoreType,DayOfWeek, Promo, StateHoliday, Year, Month, Day, Open, Weekofmonth, SchoolHoliday,
+                WeekOfYear, days_to_from_hol, Assortment, CompetitionDistance, CompetitionOpenSinceMonth,
+                CompetitionOpenSinceYear, Promo2, Promo2SinceWeek, Promo2SinceYear, PromoInterval]])
      
-    if prediction == 0:
-        pred = 'Rejected'
-    else:
-        pred = 'Approved'
-    return pred
+    return prediction
 
     # this is the main function in which we define our webpage  
 def main():       
@@ -82,13 +89,17 @@ def main():
       
     # following lines create boxes in which user can enter data required to make prediction 
     Store = st.number_input('Enter Store number/Id')
+    StoreType = st.selectbox('Select StoreType',("a","b","c","d")) 
     DayOfWeek = st.number_input('Enter DayOfWeek')
     Promo = st.selectbox('Are you in Promo',(1,0))
-    StateHoliday = st.selectbox('Is it StateHoliday',(1,0))  
+    StateHoliday = st.selectbox('Is it StateHoliday',("a","b","c"))  
+    SchoolHoliday = st.selectbox('Is it SchoolHoliday',(1,0)) 
     Year = st.number_input('Enter Year')
     Month = st.number_input('Enter Month')
     Day = st.number_input('Enter Day')
     WeekOfYear = st.number_input('Enter WeekOfYear')
+    Weekofmonth = st.number_input('Enter Weekofmonth')
+
     days_to_from_hol = st.number_input('Enter numbers of days to/from Holiday')
     Assortment = st.selectbox('Choose Assortment',("a","b","c")) 
     CompetitionDistance = st.number_input('Enter Competition distanc')
@@ -98,35 +109,17 @@ def main():
     Promo2SinceWeek = st.selectbox('Are you in Promo2 this week',(1,0))
     Promo2SinceYear = st.selectbox('Are you in Promo2 this Year',(1,0))
     result =""
+    Open = 1
+    PromoInterval = 1.0
       
     # when 'Predict' is clicked, make the prediction and store it 
     if st.button("Predict"): 
-        result = prediction(Store, DayOfWeek, Promo, StateHoliday, Year, Month, Day, WeekOfYear, 
-                            days_to_from_hol, Assortment, CompetitionDistance, CompetitionOpenSinceMonth,
-                            CompetitionOpenSinceYear, Promo2, Promo2SinceWeek, Promo2SinceYear) 
+        result = prediction(Store, StoreType, DayOfWeek, Promo, StateHoliday, Year, Month, Day, Open, Weekofmonth, SchoolHoliday,
+                            WeekOfYear, days_to_from_hol, Assortment, CompetitionDistance, CompetitionOpenSinceMonth,
+                            CompetitionOpenSinceYear, Promo2, Promo2SinceWeek, Promo2SinceYear, PromoInterval) 
         st.success('Your Sales is {}'.format(result))
         # print(LoanAmount)
      
 if __name__=='__main__': 
     main()
 
-
-# def loadData():
-#     pd.set_option('max_column', None)
-#     loaded_data = pd.read_csv('./data/train.csv')
-#     return loaded_data
-
-# def selectHandset():
-#     df = loadData()
-#     handset = st.multiselect("Choose Sore Type(s)", list(df['StoreType'].unique()))
-#     if handset:
-#         df = df[np.isin(df, handset).any(axis=1)]
-#         st.write(df)
-
-
-
-# st.markdown("<h1 style='color:#0b4eab;font-size:36px;border-radius:10px;'>Dashboard | Telecommunication Users Data Analysis </h1>", unsafe_allow_html=True)
-# selectHandset()
-# # st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
-# st.title("Data Visualizations")
-# # with st.beta_expander("Show More Graphs"):
